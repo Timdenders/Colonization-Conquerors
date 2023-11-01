ebb5143cca7ceeae8e992b66d79be52586479ce9
JoshMU1020 <118846876+JoshMU1020@users.noreply.github.com>
2023-10-31 21:05:08 -0400

    Update TilemapTest.gd

--- Latest_Build/test/Scripts/TilemapTest.gd
+++ Latest_Build/test/Scripts/TilemapTest.gd
@@ -7,8 +7,13 @@ extends GdUnitTestSuite
 # TestSuite generated from
 const __source = 'res://Scripts/Tilemap.gd'
 
+var tilemap = preload(__source).new()
+var ground_sea_layer = 0
 
 func test_check_for_alt_tile() -> void:
-	var tilemap = preload(__source).new()
 	assert_that(tilemap.check_for_alt_tile(Vector2i(1,1))).is_equal(false)
-	tilemap.free()
+
+func test_check_for_alt_tile_true() -> void:
+	var alt_tile = tilemap.get_cell_alternative_tile(ground_sea_layer, Vector2i(-19, 13))
+	assert_that(alt_tile).is_not_equal(0)
+
